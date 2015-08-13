f6435b1b91963e27860694db53f6d11d0f991874
wondywang <wondywang@users.noreply.github.com>
2015-08-13 15:18:48 +0800

    Update string_utility.h
    add hex2string

--- inc/string_utility.h
+++ inc/string_utility.h
@@ -4,6 +4,17 @@
 
 using std::string;
 
+void hex2string(const unsigned char* hex, int hexLen, char* str, int *len)
+{
+	int i, o;
+	for (i=0, o=0; i<hexLen; i++, o+=2)
+	{
+		snprintf(&str[o], sizeof(int), "%02X", hex[i]);
+	}
+	int length = 2*hexLen;
+	len = &length;
+}
+
 void HexToStr(string &sDest, const string &sSrc)
 {
 	if (sSrc.empty())
@@ -11,35 +22,33 @@ void HexToStr(string &sDest, const string &sSrc)
 		sDest = "";
 		return ;
 	}
-
 	unsigned int uLen = sSrc.length();
 	char szDest[uLen*2 + 1];
 	memset(szDest, 0, sizeof(szDest));
 	int j = 0;
-    char l_byTmp;
-    for (unsigned int i = 0; i < uLen; i++)
-    {
-        l_byTmp = (sSrc[i] & 0xf0) >> 4;
-        if (l_byTmp < 10)
-        {
-            szDest[j++] = l_byTmp + '0';
-        }
-        else
-        {
-            szDest[j++] = l_byTmp - 10 + 'A';
-        }
-        l_byTmp = sSrc[i] & 0x0f;
-        if (l_byTmp < 10)
-        {
-            szDest[j++] = l_byTmp + '0';
-        }
-        else
-        {
-            szDest[j++] = l_byTmp - 10 + 'A';
-        }
-    }
-    szDest[j] = 0;
-
+    	char l_byTmp;
+    	for (unsigned int i = 0; i < uLen; i++)
+    	{
+        	l_byTmp = (sSrc[i] & 0xf0) >> 4;
+        	if (l_byTmp < 10)
+        	{
+            		szDest[j++] = l_byTmp + '0';
+        	}
+        	else
+        	{
+            		szDest[j++] = l_byTmp - 10 + 'A';
+        	}
+        	l_byTmp = sSrc[i] & 0x0f;
+        	if (l_byTmp < 10)
+        	{
+            		szDest[j++] = l_byTmp + '0';
+        	}
+        	else
+        	{
+            		szDest[j++] = l_byTmp - 10 + 'A';
+        	}
+    	}
+    	szDest[j] = 0;
 	sDest = szDest;
 }
 
