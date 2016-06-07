2d82fc20bdcac62f48902e534ccf4b78035ef730
wondywang <wondywang@users.noreply.github.com>
2016-06-07 21:05:07 +0800

    Create test_publish_subsribe.go

---
+++ testcase/test_publish_subsribe.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"log"
+)
+
+type Observer interface {
+	Update(int)
+}
+
+type Subscribe interface {
+	Attach(Observer)
+	Detach(Observer)
+	Notify()
+}
+
+type Blog struct {
+	obs []Observer
+}
+
+func (self *Blog) Notify() {
+	for idx, item := range self.obs {
+		item.Update(idx)
+	}
+}
+
+func (self *Blog) Attach(ob Observer) {
+	self.obs = append(self.obs, ob)
+}
+
+func (self *Blog) Detach(ob Observer) {
+	for idx, item := range self.obs {
+		if item == ob {
+			self.obs = append(self.obs[0:idx], self.obs[idx+1:]...)
+			break
+		}
+	}
+}
+
+type Publish struct {
+	msg chan int
+}
+
+func (self *Publish) Update(idx int) {
+	self.msg <- idx
+}
+
+func (self *Publish) Listen() {
+	log.Println("listen publish!")
+	for {
+		select {
+		case msg := <-self.msg:
+			log.Println("get msg: ", msg)
+		default:
+//			log.Println("default case!")
+		}
+	}
+}
+
+func main() {
+	blog := &Blog{}
+
+	publisher1 := &Publish{
+		msg: make(chan int),
+	}
+	publisher1.Listen()
+
+	publisher2 := &Publish{
+		msg: make(chan int),
+	}
+	publisher2.Listen()
+
+	blog.Attach(publisher1)
+	blog.Attach(publisher2)
+
+	blog.Notify()
+
+	blog.Detach(publisher1)
+	blog.Notify()
+
+	blog.Detach(publisher2)
+	blog.Notify()
+}
